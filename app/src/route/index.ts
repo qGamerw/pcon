8860164a93d28fde9ed9e598c2b4f88d727992c9
Misha Ukhin <mish.uxin2012@yandex.ru>
2024-10-05 10:56:11 +0300

    lab 1

--- app/src/route/index.ts
+++
@@ -1,8 +0,0 @@
-import { Express, Request, Response } from 'express';
-import noteRoutes from './routes';
-
-// Функция для инициализации маршрутов
-export default function initializeRoutes(app: Express, db: any) {
-    // Вызываем функцию noteRoutes, передавая ей объект приложения и базу данных
-    noteRoutes(app, db);
-}
